58b9768b4c822eb89654242b2c4410f3dc951ad0
Slluxx <Slluxx@users.noreply.github.com>
2023-09-08 22:59:28 +0200

    Fixed .nojekyll build

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@ release = '0.2.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.githubpages']
 
 templates_path = ['_templates']
 exclude_patterns = []
