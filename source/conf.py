d85e2c69efabfe87e8d2e2d2445cc8c34f5c7edf
Slluxx <Slluxx@users.noreply.github.com>
2023-09-08 22:51:29 +0200

    Regenerate docs

--- source/conf.py
+++ source/conf.py
@@ -23,6 +23,6 @@ html_show_sourcelink = False
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-html_baseurl = "/ImPS/"
+#html_baseurl = "./ImPS/"
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
